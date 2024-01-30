b2e407561785365ce334e1fa392a3c35d9cbd650
Suriya S <suriya.sonphu@liebherr.com>
2024-01-30 21:19:32 +0700

    add db.

--- src/presentation/Travel.WebApi/Travel.WebApi/Program.cs
+++ src/presentation/Travel.WebApi/Travel.WebApi/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Travel.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<TravelDbContext>(options => options.UseSqlite("DataSource=TravelDatabase.sqlite3"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Travel Web API",
+        Version = "v1",
+        Description = "PoC Web API using .net 8 clean architecture",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Name = "Suriya S.",
+            Email = "suriyasonp.tech@gmail.com"
+        },
+    });
+});
 
 var app = builder.Build();
 
