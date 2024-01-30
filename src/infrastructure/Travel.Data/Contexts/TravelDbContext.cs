59d67f0252c6232059129bbfa83583dcb05d4347
SuriyaS. <Suriya.Sonphu@liebherr.com>
2024-01-30 17:33:34 +0700

    commit

---
+++ src/infrastructure/Travel.Data/Contexts/TravelDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travel.Domain.Entities;
+
+namespace Travel.Data.Contexts
+{
+    public class TravelDbContext : DbContext
+    {
+        public TravelDbContext(DbContextOptions<TravelDbContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<TourList>  TourLists { get; set; }
+        public DbSet<TourPackage> TourPackages { get; set; }
+    }
+}
