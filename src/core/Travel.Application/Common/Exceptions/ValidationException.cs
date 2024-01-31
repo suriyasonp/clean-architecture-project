8926ced7ba90b0173d50e8fcfe1cce2e12ec0926
Suriya S <suriya.sonphu@liebherr.com>
2024-01-31 21:01:40 +0700

    add validation exception

--- src/core/Travel.Application/Common/Exceptions/ValidationException.cs
+++ src/core/Travel.Application/Common/Exceptions/ValidationException.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Travel.Application.Common.Exceptions
 {
-    internal class ValidationException
+    public class ValidationException : Exception
     {
+        public ValidationException()
+            : base("One or more validation failure have occurred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            var failureGroups = failures
+                                .GroupBy(e => e.PropertyName, e => e.ErrorMessage);
+
+            foreach (var failureGroup in failureGroups)
+            {
+                var propertyName = failureGroup.Key;
+                var propertyFailures = failureGroup.ToArray();
+
+                Errors.Add(propertyName, propertyFailures);
+            }
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
     }
 }
